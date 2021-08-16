8856414825d7271e99d7eba37ac688480f90fa95
usbman01 <thmona53@gmail.com>
2021-08-16 16:14:01 +0200

    Add files via upload

--- ch544/inc/ch554_debug.h
+++ ch544/inc/ch554_debug.h
@@ -1,6 +1,5 @@
 #ifndef CH554_DEBUG_H_
 #define CH554_DEBUG_H_
-#include "compiler.h"   // because of CODE and XDATA macros
 /*** watchdog  ***/
 void WDTStart  (void);               /* start the watchdog with max timeout */
 void WDTStop   (void);               /* stop the watchdog */  
@@ -16,8 +15,10 @@ void WDTFeed   (unsigned char time); /* set a new timeout value for the watchdog
 #define FSYS_SELECT_750K    0x01
 #define FSYS_SELECT_187K    0x00
 unsigned int fSysSelect(unsigned char fSelector); /* return selected fSys in kHz */
-//unsigned int fSysSelectXtal (unsigned char fSelector, /* not yet implemented */
-//                             unsigned  int fXtal );
+/*
+unsigned int fSysSelectXtal (unsigned char fSelector, // not yet implemented 
+                             unsigned int  fXtal );   // xtal in kHz
+*/
 /*** delay  ***/
 void mDelaymS(unsigned char ms);  /* note: only 8 bit param */
 void mDelayuS(unsigned char us);  /* note: only 8 bit param */
@@ -29,17 +30,43 @@ void UART0Alternate(unsigned char alternate);
 void UART1Alternate(unsigned char alternate);
 
 /*** Uart       ***/  
-// todo 
-/*** misc       ***/
-unsigned long htonl (unsigned long l); /* helper for converting MSB first to LSB first*/
-unsigned int  htonw (unsigned int w);
+#define BAUD300    0x00
+#define BAUD600    0x01
+#define BAUD1200   0x02 
+#define BAUD2400   0x03 
+#define BAUD4800   0x04
+#define BAUD9600   0x05
+#define BAUD19200  0x06 
+#define BAUD38400  0x07 
+#define BAUD57600  0x08
+#define BAUD76800  0x09
+#define BAUD115200 0x0A
 
-void fastxcpy8 (unsigned char XDATA *dest, /* copy from source in xdata to dest in xdata */
-                unsigned char XDATA *src,  /* similar to memcpy limited to msx 255bytes  */
+unsigned char Uart0HiSpeed  (unsigned char reload,unsigned char smod);
+unsigned char Uart0LoSpeed  (unsigned char reload,unsigned char smod);
+unsigned char InitUart0_24M (unsigned char kBaud);
+unsigned char InitUart0_16M (unsigned char kBaud);
+unsigned char InitUart0_12M (unsigned char kBaud);
+unsigned char InitUart0_6M  (unsigned char kBaud);
+unsigned char InitUart0_3M  (unsigned char kBaud);
+unsigned char InitUart0_750k(unsigned char kBaud);
+unsigned char InitUart0(unsigned char kBaud); /* return 0xFF if the bautrate can't be reached*/
+/*** misc       ***/
+#ifndef __SDCC_mcs51                          /* not available under SDCC */
+unsigned long htonl (unsigned long l);        /* helper for converting MSB first to LSB first*/
+unsigned int  htonw (unsigned int w);
+#endif
+
+#ifdef __SDCC_mcs51                           /* map the memspecifier for  SDCC */
+  #define xdata  __xdata
+  #define code   __code
+#endif
+void fastxcpy8 (unsigned char xdata *dest,    /* copy from source in xdata to dest in xdata */
+                unsigned char xdata *src,     /* similar to memcpy limited to max 255bytes  */
                 unsigned char size);
 
-void fastccpy8 (unsigned char XDATA *dest, /* copy from source in code to dest in xdata */
-                unsigned char CODE *src,   /* similar to memcpy limited to msx 255bytes */ 
+void fastccpy8 (unsigned char xdata *dest,    /* copy from source in code to dest in xdata */
+                unsigned char code  *src,     /* similar to memcpy limited to max 255bytes */ 
                 unsigned char size);
 /*** usb ***/
 
