124a91a35154d51f4fd27393326e30a2bffeacde
usbman01 <thmona53@gmail.com>
2021-08-16 15:15:02 +0200

    Add files via upload

--- ch544/prj_sdcc/build.bat
+++ ch544/prj_sdcc/build.bat
@@ -9,10 +9,19 @@ sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\port\pwm1_alt.c
 sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\port\pwm2_alt.c
 sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\port\uart0_alt.c
 sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\port\uart1_alt.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\simpleuart.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\uart0_24M.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\uart0_16M.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\uart0_12M.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\uart0_6M.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\uart0_3M.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\uart0_750k.c
+sdcc -c --opt-code-size -o..\obj\sdcc\ ..\src\uart\initstdio.c
 
 sdas8051 -losg ..\obj\sdcc\fastxcopy.rel ..\src\misc\fastxcopy.asm
 sdas8051 -losg ..\obj\sdcc\fastccopy.rel ..\src\misc\fastccopy.asm
 
-sdar -rc ..\obj\sdcc\ch554_debug_SDCC.LIB ..\obj\sdcc\clksetup.rel ..\obj\sdcc\wdtchange.rel ..\obj\sdcc\wdtfeed.rel ..\obj\sdcc\wdtstart.rel ..\obj\sdcc\wdtstop.rel ..\obj\sdcc\delayms.rel ..\obj\sdcc\delayus.rel ..\obj\sdcc\pwm1_alt.rel ..\obj\sdcc\pwm2_alt.rel ..\obj\sdcc\uart0_alt.rel ..\obj\sdcc\uart1_alt.rel ..\obj\sdcc\fastxcopy.rel ..\obj\sdcc\fastccopy.rel
+sdar -rc ..\obj\sdcc\ch554_debug_SDCC.LIB ..\obj\sdcc\clksetup.rel ..\obj\sdcc\wdtchange.rel ..\obj\sdcc\wdtfeed.rel ..\obj\sdcc\wdtstart.rel ..\obj\sdcc\wdtstop.rel ..\obj\sdcc\delayms.rel ..\obj\sdcc\delayus.rel ..\obj\sdcc\pwm1_alt.rel ..\obj\sdcc\pwm2_alt.rel ..\obj\sdcc\uart0_alt.rel ..\obj\sdcc\uart1_alt.rel ..\obj\sdcc\fastxcopy.rel ..\obj\sdcc\fastccopy.rel ..\obj\sdcc\simpleuart.rel ..\obj\sdcc\uart0_24M.rel ..\obj\sdcc\uart0_16M.rel ..\obj\sdcc\uart0_12M.rel ..\obj\sdcc\uart0_12M.rel ..\obj\sdcc\uart0_6M.rel ..\obj\sdcc\uart0_3M.rel ..\obj\sdcc\uart0_750k.rel ..\obj\sdcc\initstdio.rel
 
 copy /Y ..\obj\sdcc\ch554_debug_SDCC.LIB ..\ch554_debug_SDCC.LIB
+pause
